27ed24ec50076a630ba1eeaa9dbfc77f5733f433
Dmitry Dolgopolov <dimafet001@gmail.com>
2019-06-30 01:04:39 -0700

    First commit

---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/HomepageActivity.java
@@ -0,0 +1,15 @@
+package com.google.android.gms.samples.vision.face.facetracker;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomepageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
---
+++ app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/LandingPageActivity.java
@@ -0,0 +1,39 @@
+package com.google.android.gms.samples.vision.face.facetracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.os.CountDownTimer;
+
+public class LandingPageActivity extends AppCompatActivity {
+    public void launchHomepage() {
+        Log.d("launchHomepage", "Launching Homepage");
+        Intent intent = new Intent(this, HomepageActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void setTimer() {
+        new CountDownTimer(1500, 500) {
+            @Override
+            public void onTick(long millisUntilFinished) {}
+
+            @Override
+            public void onFinish() {
+                launchHomepage();
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTimer();
+    }
+}
